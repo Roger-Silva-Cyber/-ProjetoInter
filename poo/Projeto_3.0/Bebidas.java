7030402f50e4510a3258908526767711b59b3c27
Rogerio Silva Cordeiro <82827351+Roger-Silva-Cyber@users.noreply.github.com>
2026-02-25 22:21:53 -0300

    Refatora cadastro e validações do projeto Java

--- poo/Projeto_3.0/Bebidas.java
+++ poo/Projeto_3.0/Bebidas.java
@@ -1,58 +1,95 @@
 import javax.swing.JOptionPane;
 
-public class Bebidas{
-  
+public class Bebidas {
 
-    //Atributos da classe
+    private static final int LIMITE_REGISTROS = 100;
+
+    // Atributos da classe
     private String produto;
     private String marca;
     private double preco;
     private int qtdEstoque = 0;
     private String peso;
-    private int id, qtd =  0;
+    private int id;
+    private int qtd = 0;
 
+    private final String[] produtoBebida = new String[LIMITE_REGISTROS];
+    private final String[] marcaBebida = new String[LIMITE_REGISTROS];
+    private final double[] precoBebida = new double[LIMITE_REGISTROS];
+    private final int[] qtdEstoqueBebida = new int[LIMITE_REGISTROS];
+    private final String[] pesoBebida = new String[LIMITE_REGISTROS];
 
-    private String[] produtoBebida = new String[100];
-    private String[] marcaBebida = new String[100];
-    private double[] precoBebida = new double[100];
-    private int[] qtdEstoqueBebida = new int[100];
-    private String[] pesoBebida = new String[100];
-   
-
-
-    //Métodos construtores
-    public Bebidas(){}
-
-    public Bebidas(int id, String marca, String peso, double preco, String produto, int qtdEstoque){
-        this.id = id;
-        this.marca = getMarca();
-        this.peso = getPeso();
-        this.preco = getPreco();
-        this.produto = getProduto();
-        this.qtdEstoque = getQtdEstoque();
-      
+    // Métodos construtores
+    public Bebidas() {
     }
 
+    public Bebidas(int id, String marca, String peso, double preco, String produto, int qtdEstoque) {
+        this.id = id;
+        this.marca = marca;
+        this.peso = peso;
+        this.preco = preco;
+        this.produto = produto;
+        this.qtdEstoque = qtdEstoque;
+    }
 
-    //Métodos de acesso
-    public String getProduto() {return produto;}
-    public void setProduto(String produto) { this.produto = produto;}
-    public String getMarca() { return marca; }
-    public void setMarca(String marca) {     this.marca = marca;}
-    public double getPreco() {  return preco;}
-    public void setPreco(double preco) {    this.preco = preco; }
-    public int getQtdEstoque() {   return qtdEstoque; }
-    public void setQtdEstoque(int qtdEstoque) { this.qtdEstoque = qtdEstoque; }
-    public String getPeso() {  return peso;}
-    public void setPeso(String peso) {  this.peso = peso; }
-    public int getId() {  return id; }
-    public void setId(int id) { this.id = id; }
-    
+    // Métodos de acesso
+    public String getProduto() {
+        return produto;
+    }
 
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
 
-    //Método para cadastro das bebidas
-    public int cadastrarBebida(){
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Método para cadastro das bebidas
+    public int cadastrarBebida() {
         int i = this.id;
+
+        if (i < 0 || i >= LIMITE_REGISTROS) {
+            JOptionPane.showMessageDialog(null, "Limite de bebidas cadastradas atingido.");
+            return qtd;
+        }
+
         produtoBebida[i] = produto;
         marcaBebida[i] = marca;
         precoBebida[i] = preco;
@@ -63,23 +100,18 @@ public class Bebidas{
         return qtd;
     }
 
-    public void relatorio( int i){      
+    public void relatorio(int i) {
+        System.out.println(
+                produtoBebida[i] + "\t\t" + marcaBebida[i] + "\t" + pesoBebida[i] + "\t" + precoBebida[i] + "\t\t"
+                        + qtdEstoqueBebida[i]);
+    }
 
-      System.out.println(produtoBebida[i]+"\t\t"+marcaBebida[i]+"\t"+ pesoBebida[i] + "\t" + precoBebida[i] + "\t\t" + qtdEstoqueBebida[i]);
-      
-        }
-
-
-
-      //Método para consulta de estoque
-      public void consultarEstoque(){
+    // Método para consulta de estoque
+    public void consultarEstoque() {
         System.out.println("\n\n \t\tBebidas disponíveis\n\n");
         System.out.println("Produto\t\tMarca\t\tPeso\tPreço\t\tQuantidade em estoque");
-        for(int i=0; i<qtd; i++){
+        for (int i = 0; i < qtd; i++) {
             relatorio(i);
         }
-        
-      }  
-
-
+    }
 }
--- poo/Projeto_3.0/Main.java
+++ poo/Projeto_3.0/Main.java
@@ -4,91 +4,108 @@ import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
-        int opcao, id = 0, idt=0;
+        int opcao;
+        int id = 0;
+        int idt = 0;
 
         Scanner sc = new Scanner(System.in);
         Bebidas b = new Bebidas();
         Cliente c = new Cliente();
         Funcionario f = new Funcionario("46978403265", 29, "Cleiton dos Santos", "Atendente", "7985");
         Funcionario f1 = new Funcionario("46978413602", 25, "João Medeiros", "Entregador", "9785");
-        
-        //Menu inicial
+
         while (true) {
-            opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Menu Principal"
+            String entrada = JOptionPane.showInputDialog(null, "Menu Principal"
                     + "\n1- Cadastrar Bebida"
                     + "\n2- Consultar Estoque"
                     + "\n3- Cadastrar Cliente"
                     + "\n4- Consultar cliente"
                     + "\n5- Funcionários"
                     + "\n6- Sair"
-                    + "\nDigite uma opção: "));
+                    + "\nDigite uma opção: ");
+
+            if (entrada == null) {
+                JOptionPane.showMessageDialog(null, "Até mais!");
+                break;
+            }
+
+            try {
+                opcao = Integer.parseInt(entrada);
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Digite apenas números válidos no menu.");
+                continue;
+            }
+
             if (opcao == 6) {
                 JOptionPane.showMessageDialog(null, "Até mais!");
                 break;
             }
+
             switch (opcao) {
-                //Menu para cadastrar uma nova bebida:
                 case 1 -> {
                     System.out.println("Cadastrar Bebida");
                     b.setId(id);
                     System.out.println("Informe o tipo de bebida:");
-                    b.setProduto(sc.next());
+                    b.setProduto(sc.nextLine());
                     System.out.println("Informe a marca:");
-                    b.setMarca(sc.next());
+                    b.setMarca(sc.nextLine());
                     System.out.println("Informe o peso(em ML) da bebida:");
-                    b.setPeso(sc.next());
+                    b.setPeso(sc.nextLine());
                     System.out.println("Informe a quantidade recebida:");
-                    b.setQtdEstoque(sc.nextInt());
+                    b.setQtdEstoque(lerInteiro(sc));
                     System.out.println("Informe o preço:");
-                    b.setPreco(sc.nextDouble());
+                    b.setPreco(lerDouble(sc));
                     id = b.cadastrarBebida();
-                    break;
                 }
                 case 2 -> {
-                    //Menu para consultar estoque de bebidas:
                     System.out.println("Consultar Bebidas:");
                     b.consultarEstoque();
-                    break;
-                    }
-
+                }
                 case 3 -> {
-                    //Menu para cadastrar um cliente:
                     System.out.println("Cadastrar Cliente");
                     c.setIdt(idt);
                     System.out.println("Digite o nome:");
-                    c.setNome(sc.next());
+                    c.setNome(sc.nextLine());
                     System.out.println("Informe a idade:");
-                    c.setIdade(sc.nextInt());
+                    c.setIdade(lerInteiro(sc));
                     System.out.println("Informe o número do CPF");
-                    c.setCpf(sc.next());
+                    c.setCpf(sc.nextLine());
                     idt = c.cadastrarCliente();
-                    break;
                 }
-
                 case 4 -> {
-                    //Menu para consultar Clientes:
                     System.out.println("Consultar Clientes:");
                     c.consultarPessoa();
-                    break;
                 }
-
                 case 5 -> {
-                    //Menu para consultar Funcionários:
                     f.consultarPessoa();
                     f1.consultarPessoa();
-
-                    break;
-                
                 }
-
-                default ->
-                    System.out.println("Opção invalida");
-
+                default -> System.out.println("Opção inválida");
             }
-
         }
 
         sc.close();
+    }
 
+    private static int lerInteiro(Scanner sc) {
+        while (true) {
+            String valor = sc.nextLine();
+            try {
+                return Integer.parseInt(valor);
+            } catch (NumberFormatException ex) {
+                System.out.println("Valor inválido. Digite um número inteiro:");
+            }
+        }
+    }
+
+    private static double lerDouble(Scanner sc) {
+        while (true) {
+            String valor = sc.nextLine().replace(',', '.');
+            try {
+                return Double.parseDouble(valor);
+            } catch (NumberFormatException ex) {
+                System.out.println("Valor inválido. Digite um número decimal:");
+            }
+        }
     }
 }
